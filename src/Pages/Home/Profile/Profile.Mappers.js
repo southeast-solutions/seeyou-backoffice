12f4eb8354917a26b4f36469086e8cacb5a13fce
georgep98 <48506690+georgep98@users.noreply.github.com>
2021-04-04 12:40:02 +0300

    update user data

--- src/Pages/Home/Profile/Profile.Mappers.js
+++ src/Pages/Home/Profile/Profile.Mappers.js
@@ -2,22 +2,8 @@ import '../../../Enums/UserTypes';
 import { userTypes } from '../../../Enums/UserTypes';
 
 const mapDataAfterUserType = (userInfo) => {
-    let user = {
-        firstName: "",
-        lastName: "",
-        phoneNumber: "",
-        city: "",
-        country: "",
-        foreignLanguages: "",
-        businessName: "",
-        businessWebsite: "",
-        cui: "",
-        adress: "",
-        socialLinks: "",
-        currentJob: "",
-        disponiblityDescription: "",
-        profileUrl: "",
-    };
+    let user = {};
+    
     if(userInfo.userType === userTypes.promoter) {
         user.socialLinks = userInfo.socialLinks;
         user.firstName = userInfo.firstName;
@@ -25,31 +11,77 @@ const mapDataAfterUserType = (userInfo) => {
         user.foreignLanguages = userInfo.foreignLanguages;
         user.city = userInfo.city;
         user.country = userInfo.country;
-        user.cui = userInfo.cui;
-        user.adress = userInfo.adress;
-        user.businessName = userInfo.businessName;
         user.phoneNumber = userInfo.phoneNumber;
-        user.userType = userInfo.userType;
+        user.userType = 'Promoter';
         user.currentJob = userInfo.currentJob;
-        user.phoneNumber = userInfo.phoneNumber;
+        user.profilePictureUrl = userInfo.profilePictureUrl;
+        user.disponibilityDescription = userInfo.disponibilityDescription;
 
         return user;
     }
 
     if(userInfo.userType = userTypes.tourOperator) {
         // Create user tour operator data
+        user.address = userInfo.tourBusinessEntity.address;
+        user.website = userInfo.tourBusinessEntity.website;
+        user.socialLinks = userInfo.tourBusinessEntity.socialLinks;
+        user.city = userInfo.tourOperatorEntity.city;
+        user.lastName = userInfo.tourOperatorEntity.lastName;
+        user.userSocialLinks = userInfo.tourOperatorEntity.socialLinks;
+        user.foreignLanguages = userInfo.tourOperatorEntity.foreignLanguages;
+        user.lastName = userInfo.tourOperatorEntity.lastName;
+        user.cui = userInfo.tourBusinessEntity.cui;
+        user.businessName = userInfo.tourBusinessEntity.businessName;
+        user.userType = "TourOperator"
+        user.profilePictureUrl = userInfo.profilePictureUrl;
+
         return user;
     }
 
     if(userInfo.userType === userTypes.contentCreator) {
-        //content creator
+        user.city = userInfo.city;
+        user.firstName = userInfo.firstName;
+        user.lastName = userInfo.lastName;
+        user.foreignLanguages = userInfo.foreignLanguages;
+        user.socialLinks = userInfo.socialLinks;
+        user.userType = "ContentCreator";
+        user.disponibilityDescription = userInfo.disponibilityDescription;
+        user.profilePictureUrl = userInfo.profilePictureUrl;
         return user;
     }
 
     if(userInfo.userType === userTypes.concierge) {
-        // concierge
-        return user;
+       user.city = userInfo.city;
+       user.country = userInfo.country;
+       user.firstName = userInfo.firstName;
+       user.lastName = userInfo.lastName;
+       user.userType = "Concierge";
+       user.foreignLanguages = userInfo.foreignLanguages;
+       user.socialLinks = userInfo.socialLinks;
+       user.profilePictureUrl = userInfo.profilePictureUrl;
+    return user;
     }
 }
 
-export { mapDataAfterUserType };
+const tourOperatorMapper = (user) => {
+    const userInfo = {};
+
+    userInfo.tourBusinessEntity.address = user.address;
+    userInfo.tourBusinessEntity.website = userInfo.website;
+    userInfo.tourBusinessEntity.socialLinks = user.socialLinks;
+    userInfo.tourOperatorEntity.city = user.city;
+    userInfo.tourOperatorEntity.lastName = user.lastName;
+    userInfo.tourOperatorEntity.userSocialLinks = user.userSocialLinks;
+    userInfo.tourOperatorEntity.foreignLanguages = user.foreignLanguages;
+    userInfo.tourOperatorEntity.lastName = user.lastName;
+    userInfo.tourBusinessEntity.cui = user.cui;
+    userInfo.tourBusinessEntity.businessName = user.businessName;
+    userInfo.tourBusinessEntity.businessName = user.businessName;
+    user.userType = "TourOperator";
+    user.profilePictureUrl = user.profilePictureUrl;
+
+    return (userInfo);
+
+}
+
+export { mapDataAfterUserType, tourOperatorMapper};
